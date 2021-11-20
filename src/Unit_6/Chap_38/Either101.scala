bda3e32e5532266020fca3dd9c4519ae00add8c2
Elvis020 <spaceyel3@gmail.com>
2021-11-20 18:10:55 +0000

    Done with Chap_38

--- src/Unit_6/Chap_38/Either101.scala
+++ src/Unit_6/Chap_38/Either101.scala
@@ -24,7 +24,8 @@ object Either101 extends App{
     Either allows you to return a value that can have one or 2 possibilities. They are Right and Left
     In Scala, Either is an immutable data structure to indicate one of 2 possible values
 
-    The Either class is a abstract and cannot be instantiated
+    The Either class is a abstract and cannot be instantiated.
+    Because, Either is sealed, when you provide an implementation for Right, you need to provide one for Left as well. If you fail to do this, the compiler will show a warning, at compile time
    */
   private def containsOnlyDigits(phoneNumber:String):Boolean = {
     val numbs: Option[Int] = phoneNumber.toIntOption
@@ -86,7 +87,144 @@ object Either101 extends App{
 
   // Pattern Matching on Either
   // Transforming an exam outcome to a message
-  def toMessage(outcome:Either[String,Pass]) = outcome match{
-    case Left(msgf) => ???
+  def toMessage(outcome:Either[String,Pass]): String = outcome match{
+    case Left(msg) => s"Fail: $msg"
+    case Right(pass) => s"Pass with score ${pass.score}"
   }
+
+  // A function that uses PM to implement sqrt of a number
+  def sqrtOrZero(n:Int): Double = n match {
+    case n if n>0 => sqrtImplementation(n).right.get
+    case _ => 0.0
+  }
+  def sqrtOrZeroAlt(n:Int): Double = sqrtImplementation(n) match {
+    case Left(_) => 0.0
+    case Right(d) => d
+  }
+
+  println()
+
+  /* Using map and flatMap implementation on Either */
+
+  // Transforming a pass to Percentage
+  case class PassP(score:Int){
+    require(score>=60 & score<=100,"Invalid pass: score must be between 60 and 100")
+    def toPercentage:Double = score/100
+  }
+  def toPercentage(outcome:Either[String,PassP]) =
+    outcome.right.map(_.toPercentage)
+  // .map on Either instance is used to transform the value wrapped into a Left or Right
+  /*
+    With the .right and .left projections, 2 things happen:
+      1. If the instance matches the selected side, it applies the the function to the value
+      2. If not, it returns the instance without performing any transformations
+   */
+
+
+  val e: Either[String, Int] = Right(42)
+  println(e.left.map(_.size)) // returns instance without any transformation
+  println(e.right.map(_ * 2.0)) // applies transformation
+
+
+  // The truncate function that truncates an input text to its first 24 chars
+  def truncate(e:Either[Double,String]): Either[Double, String] = e.map(_.substring(0,23))
+
+  def truncateAlt(e:Either[Double,String]): Either[Double, String] = e.map(_.take(23))
+
+  println(truncate(Right("Good morning, how are you doing today! Have a wonderful evening")))
+  println(truncateAlt(Right("Good morning, how are you doing today! Have a wonderful evening")))
+
+
+  /* Using flatMap */
+
+  // A function that combines 2 outcomes(grades) and finds the average
+  def combine(outcome1:Either[String,Pass],outcome2:Either[String,Pass]): Either[String, Pass] ={
+    outcome1.flatMap{
+      s1 => {
+        outcome2.map{
+          s2 =>
+            val avg = (s1.score + s2.score)/2
+            Pass(avg)
+        }
+      }
+    }
+  }
+
+
+  // Example to show how to ue flatMap on an instance of Either
+  val a:Either[String,Int] = Right(25)
+  val t: Either[Any, Double] = a.flatMap{
+    n => {
+      if(n<0) Left(-1)
+      else Right(Math.sqrt(n))
+    }
+  }
+  println(t)
+
+  val t2: Either[String, Int] = Left("").left.flatMap{
+    text => {
+      if(text.isEmpty) Right(42)
+      else Left(text)
+    }
+  }
+  println(t2)
+
+
+  // Consider this
+  def validation(a:Either[String,Int],b:Either[String,Int]) = {
+    a.flatMap{
+      aa => {
+        b.map{
+          bb => aa + bb
+        }
+      }
+    }
+  }
+
+  println(validation(Left("first failure"), Left("second failure"))) // Left("first failure") -> It wont even continue since its a Left
+  println(validation(Right(22), Right(44))) // Right(66)
+
+  println()
+
+  // Refactoring the combine function above using for-comprehension
+  def combineWithFor(outcome1:Either[String,Pass],outcome2:Either[String,Pass]): Either[String, Pass] = for{
+    o1 <- outcome1
+    o2 <- outcome2
+    avg = (o1.score+o2.score)/2
+  } yield Pass(avg)
+
+  val ex:Either[Int,String] = Left(1234)
+
+  // Using map
+  println(ex.left.map(_ + 1))
+
+  // Using for-comprehension
+  val ex2 = for{
+    v <- ex.left
+  } yield v+1
+
+  println(ex2)
+
+  // Implementing validation using for-comprehension
+  def validationWithFor(a:Either[String,Int],b:Either[String,Int]): Either[String, Int] = for{
+    aa <- a
+    bb <- b
+  } yield aa + bb
+
+  println(validationWithFor(Left("first failure"), Left("second failure")))
+
+
+  def validationWithAccumulatedErrors(a:Either[String,Int],b:Either[String,Int]): Either[List[String], Int] =
+    a match {
+      case Left(value1a) =>
+      b match {
+        case Left(value1b) => Left(List(value1a,value1b))
+      }
+      case Right(value2a) => b match {
+        case Right(value2b) => Right(value2a + value2b)
+      }
+    }
+
+  println(validationWithAccumulatedErrors(Left("first failure"), Left("second failure")))
+  println(validationWithAccumulatedErrors(Right(22), Right(44)))
 }
