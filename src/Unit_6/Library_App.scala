9730650cda8fadba8db4f6205a8257d3aaed8f44
Elvis020 <spaceyel3@gmail.com>
2021-11-22 22:36:51 +0000

    Updated

---
+++ src/Unit_6/Library_App.scala
@@ -0,0 +1,16 @@
+package Unit_6
+
+/**
+ * Project: Scala_with_Daniela
+ * Package: Unit_6
+ *
+ * User: elvis 😎😎
+ * Date: 21/11/2021
+ * Time: 22:49
+ *
+ * Happy Coding
+ */
+object Library_App extends App{
+  println("Check Project Library for more information about this chap")
+
+}
